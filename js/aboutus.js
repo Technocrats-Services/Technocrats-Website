8a76fe6c8f34d6516169dac47f96745ff2bb24d1
Nitish Kumar Dubay <75241307+zedan101@users.noreply.github.com>
2021-03-14 00:13:06 -0800

    Add files via upload

--- js/aboutus.js
+++ js/aboutus.js
@@ -2,20 +2,50 @@ $(document).ready(function(){
     $(window).scroll(function(){
         var positiontop = $(document).scrollTop();
         console.log(positiontop);
-
-        if(positiontop>300 && positiontop<600){
-        $('.bxc2').addClass('anim')
+        if(screen.width<700){
+            if(positiontop>100 && positiontop<300){
+                $('.bxc2').addClass('anim')
+                }
+            
+                if(positiontop>300 && positiontop<600){
+                $('.bxc3').addClass('anim')
+                }
+                if(positiontop>600 && positiontop<900){
+                $('.bxc4').addClass('anim')
+                }
+                if(positiontop>900 && positiontop<1200){
+                $('.bxc5').addClass('anim')
+                }
+        
+        
         }
-       
-        if(positiontop>800 && positiontop<1200){
-        $('.bxc3').addClass('anim')
-        }
-        if(positiontop>1500 && positiontop<2000){
-        $('.bxc4').addClass('anim')
-        }
-        if(positiontop>2000 && positiontop<2500){
-        $('.bxc5').addClass('anim')
+        else{    
+            if(positiontop>300 && positiontop<600){
+            $('.bxc2').addClass('anim')
+            }
+        
+            if(positiontop>800 && positiontop<1200){
+            $('.bxc3').addClass('anim')
+            }
+            if(positiontop>1500 && positiontop<2000){
+            $('.bxc4').addClass('anim')
+            }
+            if(positiontop>2000 && positiontop<2500){
+            $('.bxc5').addClass('anim')
+            }
         }
 
     });
 });
+window.onload=function(){
+let floN = document.getElementById('floN')
+
+floN.addEventListener('click',function hover(){
+    let instaN = document.getElementById('instaN')
+    let imgN = document.getElementById('imgN')
+    console.log('run')
+    instaN.addClass('dis')
+    imgN.addClass('imgblr')
+
+});
+}
